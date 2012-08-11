3511af4d79138d002962e42e720a406e14e54d29
Zoxc <zoxc32@gmail.com>
2012-08-11 19:37:09 +0200

    Renamed prev_rsp.

--- src/x86_64/arch.hpp
+++ src/x86_64/arch.hpp
@@ -10,7 +10,7 @@ namespace Arch
 		uint64_t ds;                  // Data segment selector
 		uint64_t r11, r10, r9, r8, rdi, rsi, rdx, rcx, rax; // Pushed by pusha.
 		uint64_t interrupt_index, error_code;    // Interrupt number and error code (if applicable)
-		uint64_t rip, cs, rflags, prev_rsp, ss; // Pushed by the processor automatically.
+		uint64_t rip, cs, rflags, rsp, ss; // Pushed by the processor automatically.
 	} __attribute__((packed));
 
 	struct Registers
